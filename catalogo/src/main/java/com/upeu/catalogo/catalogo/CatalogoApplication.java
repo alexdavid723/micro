0b272850e963a17ca92b7923dc012df8beec3a4d
noetipo <noe.tipo@gmail.com>
2023-05-06 19:24:09 -0500

    configuraciones

--- catalogo/src/main/java/com/upeu/catalogo/catalogo/CatalogoApplication.java
+++ catalogo/src/main/java/com/upeu/catalogo/catalogo/CatalogoApplication.java
@@ -6,9 +6,13 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
+
+
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CatalogoApplication {
 
     public static void main(String[] args) {
