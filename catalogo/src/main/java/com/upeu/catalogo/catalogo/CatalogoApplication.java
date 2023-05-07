52fe07b664970cdead9ea31067587bbb03210945
noetipo <noe.tipo@gmail.com>
2023-05-06 19:49:30 -0500

    correccion

--- catalogo/src/main/java/com/upeu/catalogo/catalogo/CatalogoApplication.java
+++ catalogo/src/main/java/com/upeu/catalogo/catalogo/CatalogoApplication.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
