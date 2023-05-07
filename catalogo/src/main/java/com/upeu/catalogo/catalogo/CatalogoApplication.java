e90d752339f302d194f4d1bee758dda9acd4f774
noetipo <noe.tipo@gmail.com>
2023-05-06 22:28:20 -0500

    hystrix

--- catalogo/src/main/java/com/upeu/catalogo/catalogo/CatalogoApplication.java
+++ catalogo/src/main/java/com/upeu/catalogo/catalogo/CatalogoApplication.java
@@ -13,7 +13,6 @@ import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class CatalogoApplication {
 
     public static void main(String[] args) {
